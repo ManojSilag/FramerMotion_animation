e58acae8d47430b9ad3831785e0ffc407b510ba3
Manoj Silag <silagmanoj@yahoo.in>
2020-07-18 19:28:13 +0530

    Add files via upload

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,34 +1,90 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1.5,
+      duration: 1.5
+    }
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" }
+  }
+};
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw",
+    opacity: 0
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      stiffness: "120"
+    }
+  }
+};
+
+const bottomVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    BoxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      yoyo: Infinity,
+      duration: 0.3
+    }
+  }
+};
 
 const Base = ({ addBase, pizza }) => {
-  const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
-    <div className="base container">
-
+    <motion.div
+      className="base container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
-        {bases.map(base => {
-          let spanClass = pizza.base === base ? 'active' : '';
+        {bases.map((base) => {
+          let spanClass = pizza.base === base ? "active" : "";
           return (
-            <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
-            </li>
-          )
+            <motion.li
+              key={base}
+              onClick={() => addBase(base)}
+              whileHover={{ scale: 1.3, originX: 0, color: "#f8e112" }}
+              transition={{ style: "spring", stiffness: "300" }}
+            >
+              <span className={spanClass}>{base}</span>
+            </motion.li>
+          );
         })}
       </ul>
 
       {pizza.base && (
-        <div className="next">
+        <motion.div className="next" variants={nextVariants}>
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button variants={bottomVariants} whileHover="hover">
+              Next
+            </motion.button>
           </Link>
-        </div>
+        </motion.div>
       )}
-
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Base;
--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,32 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+const containerVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1.5,
+      duration: 1.5
+    }
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" }
+  }
+};
+
+const bottomVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    BoxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      yoyo: Infinity,
+      duration: 0.3
+    }
+  }
+};
 
 const Toppings = ({ addTopping, pizza }) => {
-  let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
+  let toppings = [
+    "mushrooms",
+    "peppers",
+    "onions",
+    "olives",
+    "extra cheese",
+    "tomatoes"
+  ];
 
   return (
-    <div className="toppings container">
-      
+    <motion.div
+      className="toppings container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
       <h3>Step 2: Choose Toppings</h3>
       <ul>
-        {toppings.map(topping => {
-          let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
+        {toppings.map((topping) => {
+          let spanClass = pizza.toppings.includes(topping) ? "active" : "";
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
-              <span className={spanClass}>{ topping }</span>
-            </li>
-          )
+            <motion.li
+              key={topping}
+              onClick={() => addTopping(topping)}
+              whileHover={{ scale: 1.3, originX: 0, color: "#f8e112" }}
+              transition={{ style: "spring", stiffness: "300" }}
+            >
+              <span className={spanClass}>{topping}</span>
+            </motion.li>
+          );
         })}
       </ul>
 
       <Link to="/order">
-        <button>
+        <motion.button variants={bottomVariants} whileHover="hover">
           Order
-        </button>
+        </motion.button>
       </Link>
-
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Toppings;
